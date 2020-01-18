d5f8a6fdb5a4c39ef27614d1d94790194fcfb76f
Dithesh <58114192+Dithesh@users.noreply.github.com>
2020-01-19 00:11:06 +0530

    upload
    upload
    
    Co-Authored-By: godreamt <godreamt@users.noreply.github.com>

---
+++ includes/header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=0">
+    <meta name="HandheldFriendly" content="true" />
+
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="title" content="">
+    <!-- <link rel="icon" href=""> -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap-grid.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap-reboot.min.css" crossorigin="anonymous">
+
+    <!-- owl slider -->
+    <!-- <link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/owl-carousel/owl.theme.default.css" crossorigin="anonymous"> -->
+
+    <!-- <link rel="stylesheet" type="text/css" href="css/animate.css"> -->
+    <!-- aos animation -->
+    <!-- <link rel="stylesheet" type="text/css" href="css/aos.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="css/jquery.simplyscroll.css"> -->
+    <!-- google font -->
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+    <title>Vistaar</title>
+</head>
+
+<body>
+    <div class="header_menuOuter">
+        <div class="container">
+            <div class="header_nav">
+                <div class="nav_brandBox">
+                    <img src="images/logo.png" class="logo_img img-fluid" alt="">
+                </div>
+                <div class="nav_menuBar ml-auto">
+                    <ul class="list-unstyled">
+                        <li><a href="#">Partner with Us</a></li>
+                        <li><a href="#">Loan Enquiry</a></li>
+                        <li><a href="#">080-30088494</a></li>
+                    </ul>
+                </div>
+                <div class="social_menuBox">
+                    <a href="#"><img src="images/facebook.png" alt=""></a>
+                    <a href="#"><img src="images/linkedin.png" alt=""></a>
+                    <a href="#"><img src="images/youtube.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
