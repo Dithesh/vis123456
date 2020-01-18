672a8a432d6b8e121f795f77e4fa11f48aedb906
godreamt <48436978+godreamt@users.noreply.github.com>
2020-01-19 00:46:37 +0530

    Create index-v2.php

---
+++ index-v2.php
@@ -0,0 +1,110 @@
+<?php include('includes/header.php'); ?>
+
+<div class="home_bannerBox">
+    <img src="images/dots_group.png" class="banner_designImage donts1" alt="">
+    <img src="images/dots_group.png" class="banner_designImage donts2" alt="">
+    <img src="images/circle.png" class="banner_designImage circle1" alt="">
+    <img src="images/circle.png" class="banner_designImage circle2" alt="">
+    <img src="images/arrow.png" class="banner_designImage arrow1" alt="">
+    <div class="container">
+        <div class="top_middleLine mx-auto"></div>
+        <div class="banner_textBox text-center">
+            <!-- <h4>EMPOWERING SMALL</h4> -->
+            <h3>BUSINESSES</h3>
+        </div>
+        <div class="tag_lineBox text-center">
+            <img src="images/text-box.png" class="img-fluid" alt="">
+        </div>
+        <div class="bottom_middleLine mx-auto"></div>
+        <div class="banner_menuBoxRow">
+            <div class="row">
+                <div class="col-lg col-md-3">
+                    <div class="bannerMenu_box">
+                        <a href="#" class="bannerMenu_inner">
+                            <img src="images/small-arrow.png" class="arrow_img" alt="">
+                            <span>About Us</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg col-md-3">
+                    <div class="bannerMenu_box">
+                        <a href="#" class="bannerMenu_inner">
+                            <img src="images/small-arrow.png" class="arrow_img" alt="">
+                            <span>Products</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg col-md-3">
+                    <div class="bannerMenu_box">
+                        <a href="#" class="bannerMenu_inner">
+                            <img src="images/small-arrow.png" class="arrow_img" alt="">
+                            <span>Our Customers</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg col-md-3">
+                    <div class="bannerMenu_box">
+                        <a href="#" class="bannerMenu_inner">
+                            <img src="images/small-arrow.png" class="arrow_img" alt="">
+                            <span>Working At Vistaar</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg col-md-3">
+                    <div class="bannerMenu_box">
+                        <a href="#" class="bannerMenu_inner">
+                            <img src="images/small-arrow.png" class="arrow_img" alt="">
+                            <span>Investor Relations</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg col-md-3">
+                    <div class="bannerMenu_box">
+                        <a href="#" class="bannerMenu_inner">
+                            <img src="images/small-arrow.png" class="arrow_img" alt="">
+                            <span>News</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg col-md-3">
+                    <div class="bannerMenu_box">
+                        <a href="#" class="bannerMenu_inner">
+                            <img src="images/small-arrow.png" class="arrow_img" alt="">
+                            <span>Contact</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="portfolio_section">
+    <img src="images/blue_dots_group.png" class="section_graphicImage blue_dots" alt="">
+    <img src="images/arrow.png" class="section_graphicImage arrow_right" alt="">
+    <img src="images/lines_box.png" class="section_graphicImage lines_btm" alt="">
+    <img src="images/circle.png" class="section_graphicImage circle_img" alt="">
+    <div class="container">
+        <div class="portfolio_inner">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div class="section_titleBox mb-5 pb-5">
+                        <h2>AT A GLANCE</h2>
+                    </div>
+                    <div class="section_detailBox">
+                        <h5>Vistaar is a commitment to create new economic opportunities for</h5>
+                        <p>deserving small business women and men, enriching lives and transforming communities. The company focuses on the missing middle segment, which is not effectively served by the formal financial system. Vistaar works to make available appropriately designed financial services, for this segment alone. This is nation building done right, one entrepreneur at a time. </p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-12">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
+    
+    
+
+    
