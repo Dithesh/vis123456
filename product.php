2a2fcee67f47cc2d20f2736af6c7cf325a5551d9
Dithesh <58114192+Dithesh@users.noreply.github.com>
2020-01-23 19:09:05 +0530

    Slider Update

--- product.php
+++ product.php
@@ -1,14 +1,32 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/navbar.php'); ?>
 
-<div class="product_heroBannerOuter">
+<section class="product_heroBannerOuter">
     <div class="owl-carousel productSlider owl-theme">
         <div class="item">
             <div class="hero_imageBox"></div>
         </div>
     </div>
-    <div class="hero_outerStrip"></div>
-</div>
+    <div class="hero_outerStrip">
+        <h5></h5>
+        <p></p>
+    </div>
+</section>
+<section class="solutions_section">
+    <div class="container solutionsInner">
+        <div class="row">
+            <div class="col-lg-3 col-md-12">
+            
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+                    
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+            
+            </div>
+        </div>
+    </div>
+</section>
 
 <section class="footerTop_section">
     <?php include('includes/footermenu.php'); ?>
